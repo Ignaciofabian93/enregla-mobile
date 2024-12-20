184c87f8ccb2c791b581b10a4ecc05995aef6e96
unknown <ignaciofabian93@gmail.com>
2024-12-19 21:42:35 -0300

    endpoint

--- services/endpoint.ts
+++ services/endpoint.ts
@@ -1,6 +1,6 @@
-const protocol: string = "http";
+const protocol: string = "https";
 const port: number = 4000;
-const ip: string = "192.168.1.86";
-// const ip: string = "www.enreglaintegral.cl";
+// const ip: string = "192.168.1.86";
+const ip: string = "www.enreglaintegral.cl";
 
 export const endpoint = `${protocol}://${ip}:${port}/api`;
